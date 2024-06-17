9249ce47fbc8cbad23590be6195d1dfb0fb314bb
Hitesh Kumar <hitesh25kumar@gmail.com>
2024-06-17 15:43:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {}
+
+config.endpoint = 'https://cosmosrgeastuscceb6ff1-37de-4fe7-af6fdb.documents.azure.com:443/'
+config.key = ''
+
+config.database = {
+  id: 'ToDoList1'
+}
+
+config.container = {
+  id: 'Items1'
+}
+
+module.exports = config
